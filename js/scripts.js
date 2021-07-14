873761a9aca97495f3c36a7a151ebefed193167d
Isuru Nanayakkara <isuru@cenango.lk>
2021-07-14 12:18:44 +0530

    CORS workaround

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,28 @@ var interval = setInterval(function() {
 }, 100);
 
 function getVaccineCentersList() {
-    const Url = "https://mysterious-eyrie-27080.herokuapp.com/https://vaccine.covid19.gov.lk/cz-portal-backend-service/api/v1/vsession/list"
+    // const Url = "https://vaccine.covid19.gov.lk/cz-portal-backend-service/api/v1/vsession/list"
 
-    fetch(Url)
-    .then(response => response.json())
-    .then(data => console.log(data));
+    // fetch(Url, {
+    //     method: 'GET',
+    //     headers: { 
+    //         'Content-Type': 'application/json',
+    //         'X-Requested-With': 'XMLHttpRequest'
+    // }})
+    // .then(response => response.json())
+    // .then(data => console.log(data));
+    
+    const Url = "https://mysterious-eyrie-27080.herokuapp.com/https://vaccine.covid19.gov.lk/cz-portal-backend-service/api/v1/vsession/list"
+    
+    fetch(Url, {
+      "method": "GET",
+      "headers": {
+            "X-Requested-With": "XMLHttpRequest"
+      }
+    })
+    .then(response => {
+        if (response.ok) return response.json()
+        throw new Error('Network response was not ok.')
+    })
+    .then(data => console.log(data.contents));
 }
