d2a7d25bfb4d4e52be4f04eafca91361b9edbdd1
Isuru Nanayakkara <isuru@cenango.lk>
2021-07-14 09:58:08 +0530

    Disable CORS

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,9 @@ var interval = setInterval(function() {
 function getVaccineCentersList() {
     const Url = "https://vaccine.covid19.gov.lk/cz-portal-backend-service/api/v1/vsession/list"
 
-    fetch(Url)
+    fetch(Url, {
+        mode: 'no-cors'
+    })
     .then(data => { return data.json()})
     .then(res => { console.log(res) })
 }
