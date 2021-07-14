da640a46648d2f506c5408758c11de92143c215d
Isuru Nanayakkara <isuru@cenango.lk>
2021-07-14 14:00:38 +0530

    Setup new CORS proxy server on heroku

--- js/scripts.js
+++ js/scripts.js
@@ -6,25 +6,9 @@ var interval = setInterval(function() {
 }, 100);
 
 function getVaccineCentersList() {
-    // const Url = "https://vaccine.covid19.gov.lk/cz-portal-backend-service/api/v1/vsession/list"
+    const Url = "https://baroque-chaise-38790.herokuapp.com/https://vaccine.covid19.gov.lk/cz-portal-backend-service/api/v1/vsession/list"
 
-    // fetch(Url, {
-    //     method: 'GET',
-    //     headers: { 
-    //         'Content-Type': 'application/json',
-    //         'X-Requested-With': 'XMLHttpRequest'
-    // }})
-    // .then(response => response.json())
-    // .then(data => console.log(data));
-    
-    const Url = "https://mysterious-eyrie-27080.herokuapp.com/https://vaccine.covid19.gov.lk/cz-portal-backend-service/api/v1/vsession/list"
-    
-    fetch(Url, {
-      "method": "GET",
-      "headers": {
-            "X-Requested-With": "XMLHttpRequest"
-      }
-    })
+    fetch(Url)
     .then(response => {
         if (response.ok) return response.json()
         throw new Error('Network response was not ok.')
