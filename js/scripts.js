1864ab1e4e8acda1ca5e24c192e3138d59e13679
Isuru Nanayakkara <isuru@cenango.lk>
2021-07-14 11:27:12 +0530

    Running the API call through proxy server

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,9 @@ var interval = setInterval(function() {
 }, 100);
 
 function getVaccineCentersList() {
-    const Url = "https://vaccine.covid19.gov.lk/cz-portal-backend-service/api/v1/vsession/list"
+    const Url = "https://mysterious-eyrie-27080.herokuapp.com/https://vaccine.covid19.gov.lk/cz-portal-backend-service/api/v1/vsession/list"
 
-    fetch(Url, {
-        mode: 'no-cors'
-    })
-    .then(data => { return data.json()})
-    .then(res => { console.log(res) })
+    fetch(Url)
+    .then(response => response.json())
+    .then(data => console.log(data));
 }
