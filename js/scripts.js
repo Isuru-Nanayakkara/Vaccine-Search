428902f3578a40eaed4e5c5ee316e2415c03ad32
Isuru Nanayakkara <isuru@cenango.lk>
2021-07-15 09:51:06 +0530

    Pull down data form API and show it in a bare bones table
    Disabled CORS using a browser extension for now

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,23 @@ var interval = setInterval(function() {
 }, 100);
 
 function getVaccineCentersList() {
-    // const Url = "https://baroque-chaise-38790.herokuapp.com/https://vaccine.covid19.gov.lk/cz-portal-backend-service/api/v1/vsession/list"
-    const Url = "https://baroque-chaise-38790.herokuapp.com/https://google.com/"
+    // const Url = `https://baroque-chaise-38790.herokuapp.com/${encodeURI('https://vaccine.covid19.gov.lk/cz-portal-backend-service/api/v1/vsession/list')}`
+    const Url = 'https://vaccine.covid19.gov.lk/cz-portal-backend-service/api/v1/vsession/list'
 
     fetch(Url)
-    .then(response => {
-        console.log(response);
-        // if (response.ok) return response.json()
-        // throw new Error('Network response was not ok.')
-    })
-    // .then(data => console.log(data.contents));
+    .then(response => response.json())
+    .then(data => { 
+        for(const obj of data) {
+            let row = document.createElement('tr');
+            row.appendChild(document.createElement('td')).textContent = obj.date;
+            row.appendChild(document.createElement('td')).textContent = obj.dose;
+            row.appendChild(document.createElement('td')).textContent = obj.ageGroup;
+            row.appendChild(document.createElement('td')).textContent = obj.vaccineType;
+            row.appendChild(document.createElement('td')).textContent = obj.mohCenter;
+
+            const table = document.getElementsByClassName('table')[0];
+            table.appendChild(row);
+        }
+     })
+    .catch(error => console.log(error))
 }
